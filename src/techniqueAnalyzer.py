469aa1c04f6cc3ea4bd75acfa4cb7781f7d42317
Iain Slim <iain.j.slim@outlook.com>
2025-02-22 15:28:32 -0500

    quick commit before pull

--- src/techniqueAnalyzer.py
+++ src/techniqueAnalyzer.py
@@ -6,5 +6,11 @@ from identifyPaddlingSide import identifyPaddlingSide
 def techniqueAnalyzer(trc_file, mot_file):
     right_side = identifyPaddlingSide(trc_file)
 
+    # non-side data
+    
+    # top-side data
+
+    # bottom-side data
+
     # reassign left/right to top/bottom accordingly based on right_side
     return
